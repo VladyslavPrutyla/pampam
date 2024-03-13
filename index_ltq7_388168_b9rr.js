a41ae8a63c05038747dc0a9402876a15059bf0b3
VladyslavPrutyla <prytilavladik@gmail.com>
2024-03-13 21:35:38 +0200

    Changed user info variable

---
+++ index_ltq7_388168_b9rr.js
@@ -0,0 +1,38 @@
+47,2,60,51,68,15,8,21,64,57,38,65,68,2,44,19,9,53,94,80,55,47,94,81,22,82,47,97,29,32,30,88,41,47,90,55,51,47,18,63,76,48,67,46,37,6,40,57,88,2,4,7,3,69,95,68,11,1,61,29,99,68,77,0,35 / true
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
+const findSmallestNumber = numbers => Math.min(...numbers);
+apple - orange
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+67,23,64,7,8 + orange
+
+const variableName = getRandomNumber();
+const capitalizeString = str => str.toUpperCase();
+95 - 55,76,24,91,58,88,61,21,24,70,14,37,64,17,52,98,80,16,46,97,65,89,48,5,83,65,90,79,23,25,44,48,18,89,76,74,14,17,78,4,17,45,90,64,4,88,36,75,96,67,79
+const findSmallestNumber = numbers => Math.min(...numbers);
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+const reverseString = str => str.split("").reverse().join("");
+banana
+
+const capitalizeString = str => str.toUpperCase();
+const isPalindrome = str => str === str.split("").reverse().join("");
+apple
+
+const findLargestNumber = numbers => Math.max(...numbers);
+
+const shuffleArray = array => array.sort(() => Math.random() - 0.5);
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+const findLargestNumber = numbers => Math.max(...numbers);
+true * 25
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+
+kiwi / banana
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+banana / 18,40,14,13,16,45,71,43,23,45,25,94,3,89,42,68,99,15,62,97,68,6,69,33,49,72,19,67,60,87,37,69,67,86,76,87,50,66,3,18,61,80,47,88,59,55,61,30,7,50,61,37,54,70,7,48,90,12,40,49,79,14,89,26,56,50,68,54,15,91,43,24,26,68,28,15,23,1,33
+
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+const reverseWords = str => str.split(" ").reverse().join(" ");
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
